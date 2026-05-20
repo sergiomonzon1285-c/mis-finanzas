9e96bcee4769b8116a5e73d3c300f767bebc4f02
Sergio Monzon <sergiomonzon1285@gmail.com>
2026-05-20 11:20:40 -0300

    fix supabase key

--- src/supabase.js
+++ src/supabase.js
@@ -1,8 +1,10 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://fkkwljarmfqneoolntyi.supabase.co'
+const supabaseUrl =
+  'https://fkkwljarmfqneoolntyi.supabase.co'
 
-const supabaseKey = 'sb_publishable_xy_jEvZZ9cBUt3JuV-ZYFw_hqwDi_2c'
+const supabaseKey =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZra3dsamFybWZxbmVvb2xudHlpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzkxOTg0MDksImV4cCI6MjA5NDc3NDQwOX0.q4fftq783lxfiZ8bqcH-8lm9YK2sdQdqq6RNxgzsP7w'
 
 export const supabase = createClient(
   supabaseUrl,
